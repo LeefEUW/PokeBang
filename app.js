b7148223dd36a762f12fad459a1c3db6a8c7eab0
Leef <mehdi.diouane31@gmail.com>
2022-07-12 14:32:59 +0200

    template done

---
+++ app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            pvPlayer: 100,
+            pvEnnemy: 100
+        };
+    },
+    methods: {
+        attackPlayer() {
+
+        },
+        attackSpePlayer() {
+
+        },
+        soinPlayer() {
+
+        },
+        ennemyAttack() {
+
+        },
+        giveUp() {
+
+        }
+    }
+});
+app.mount('#app');
