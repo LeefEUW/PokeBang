d30f264badd3f69fcb83a5652af1633a08fe8d6b
Leef <mehdi.diouane31@gmail.com>
2022-07-13 10:57:09 +0200

    temporary commit

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
 const app = Vue.createApp({
     data() {
         return {
-<<<<<<< HEAD
             pvPlayer: 1500,
             pvEnnemy: 1500
-=======
-            pvPlayer: 100,
-            pvEnemy: 100
->>>>>>> 8c541719e63277754371bf96fffee89227de0d4c
         };
     },
     methods: {
-        attack(num1,num2) {
-           this.pvEnnemy -= num1;
-           this.pvPlayer -= num2;
+        attack(num1, num2) {
+            this.pvEnnemy -= num1;
+            this.pvPlayer -= num2;
         },
-        
+
         attackSpePlayer() {
 
         },
         soinPlayer() {
-<<<<<<< HEAD
-           this.pvPlayer = Math.floor(Math.random() * 1500) + 20;
-                
-=======
+            this.pvPlayer = Math.floor(Math.random() * 1500) + 20;
+
             math.random
         },
         ennemyAttack() {
 
->>>>>>> 8c541719e63277754371bf96fffee89227de0d4c
         },
-        
-       
-    
+
+
+
         giveUp() {
 
         }
